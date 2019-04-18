da72aef679b68ef4e2093b6f5ce2113482f2fb96
AndrejKubanda <34286841+AndrejKubanda@users.noreply.github.com>
2019-04-18 11:58:36 +0200

    IA: implement State::update

--- include/dg/analysis/PointsTo/InvalidatedAnalysis.h
+++ include/dg/analysis/PointsTo/InvalidatedAnalysis.h
@@ -10,16 +10,36 @@ namespace analysis {
 namespace pta {
 
 class InvalidatedAnalysis {
+    using PSNodePtrSet = std::set<PSNode *>;
+
     struct State {
-        std::set<PSNode *> mustBeInv{};
-        std::set<PSNode *> mayBeInv{};
+        PSNodePtrSet mustBeInv{};
+        PSNodePtrSet mayBeInv{};
         /*
         std::set<PSNode *> mustBeNull;
         std::set<PSNode *> cannotBeNull;
         */
+
+        // node is changed when its State has been updated
+        bool update(State* predState) {
+            unsigned mustSizeBefore = mustBeInv.size();
+            unsigned maySizeBefore = mayBeInv.size();
+
+            // MUST: intersection
+            PSNodePtrSet tmp;
+            std::set_intersection(mustBeInv.begin(), mustBeInv.end(),
+                    predState->mustBeInv.begin(), predState->mustBeInv.end(),
+                    std::inserter(tmp, tmp.end()));
+            std::swap(tmp, mustBeInv);
+
+            // MAY: union
+            mayBeInv.insert(predState->mayBeInv.begin(), predState->mayBeInv.end());
+
+            return mustSizeBefore != mustBeInv.size() || maySizeBefore != mayBeInv.size();
+        }
     };
 
-    PointerSubgraph *PS{nullptr};
+    PointerSubgraph* PS { nullptr };
 
     // mapping from PSNode's ID to States
     std::vector<State *> _mapping;
@@ -29,8 +49,14 @@ class InvalidatedAnalysis {
         return node->getType() == PSNodeType::STORE ||
                node->getType() == PSNodeType::ALLOC ||
                node->getType() == PSNodeType::DYN_ALLOC ||
-               node->getType() == PSNodeType::INVALIDATE_LOCALS ||;
-               node->getType() == PSNodeType::INVALIDATE_OBJECT;
+               node->getType() == PSNodeType::FREE;
+               /* || node->getType() == PSNodeType::INVALIDATE_LOCALS ||
+               node->getType() == PSNodeType::INVALIDATE_OBJECT;*/
+    }
+
+    // Added temporarily. ( Currently I am focused only on FREE instruction. )
+    static inline bool isFreeType(PSNode *node) {
+        return node->getType() == PSNodeType::FREE;
     }
 
     static inline bool noChange(PSNode *node) {
@@ -38,6 +64,7 @@ class InvalidatedAnalysis {
                 !isRelevantNode(node);
     }
 
+    // crates new State in _states and creates a pointer to it in _mapping
     State *newState(PSNode *nd) {
         assert(nd->getID() < _mapping.size());
 
@@ -59,9 +86,24 @@ class InvalidatedAnalysis {
                isRelevantNode(node);
     }
 
-    bool processNode(PSNode *node) {
-        assert(node->getID() <_states.size());
+    // at the moment this method is not optimal. It will be useful later.
+    bool decideMustOrMay(PSNode* node, PSNode* target) {
+        size_t pointsToSize = node->pointsTo.size();
+        bool changed = false;
+        if (pointsToSize == 1) {
+            // must
+            getState(node)->mustBeInv.insert(target);
+        } else if (pointsToSize > 1) {
+            // may
+            getState(node)->mayBeInv.insert(target);
+        }
+        return changed;
+    }
 
+    bool processNode(PSNode *node) {
+        assert(node);
+        assert(node->getID() < _states.size());
+        // if node has not changed -> node now shares a state with its predecessor
         if (noChange(node)) {
             auto pred = node->getSinglePredecessor();
             assert(pred->getID() <_states.size());
@@ -70,36 +112,45 @@ class InvalidatedAnalysis {
             return false;
         }
 
-        if (changesState(node) && !getState(node)) {
-            newState(node);
+        // no need to create new states when we initialize states, mapping in constructor
+        // if (changesState(node) && !getState(node)) { newState(node); }
+        bool changed = false;
+
+        for (PSNode* pred : node->getPredecessors()) {
+            changed |= getState(node)->update(getState(pred));
         }
 
-        for (PSNode *pred : node->getPredecessors()) {
+        if (isFreeType(node)) {
+            for (const auto& ptrStruct : node->pointsTo) {
+                changed |= decideMustOrMay(node, ptrStruct.target);
+            }
         }
-
-        return false;
+        return changed;
     }
 
 public:
 
-    /// Pointer subgraph with computed pointers
-    InvalidatedAnalysis(PointerSubgraph *ps)
-    : PS(ps), _states(ps->size()) {}
+    explicit InvalidatedAnalysis(PointerSubgraph *ps)
+    : PS(ps), _mapping(ps->size()), _states(ps->size()) {
+        for (size_t i = 0; i < ps->size(); ++i) {
+            _mapping[i] = _states[i].get();
+        }
+    }
 
     void run() {
         std::vector<PSNode *> to_process;
         std::vector<PSNode *> changed;
 
-        to_process.resize(PS->size());
-        for (auto& nd : PS->getNodes())
+        // node[0] is null so indices of nodes correspond to their IDs
+        for (auto& nd : PS->getNodes()) {
             to_process.push_back(nd.get());
+        }
 
         while(!to_process.empty()) {
-            for (auto nd : to_process) {
-                if (processNode(nd))
+            for (auto* nd : to_process) {
+                if (nd && processNode(nd))
                     changed.push_back(nd);
             }
-
             to_process.swap(changed);
             changed.clear();
         }
@@ -112,3 +163,4 @@ public:
 } // namespace dg
 
 #endif // _DG_INVALIDATED_ANALYSIS_H_
+        
